3468b4d40423394242b0ac02f25ba66e2f30e540
Eric Sorum <esorum@sgcmail.com>
2024-12-28 14:35:54 -0600

    Started entering data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './output.css'
+import MainForm from './components/main-form/mainform'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <h1>Keg Curator</h1>
+      <h2>Find the best local beer to serve in your bar or restaurant.</h2>
+      {/* <Button variant="outline">Button</Button>
+      <ExampleSelect /> */}
+      <hr></hr>
+      <MainForm />
     </>
   )
 }
---
+++ src/components/main-form/mainform.tsx
@@ -0,0 +1,13 @@
+import { ExampleSelect } from './select/ExampleSelect'
+
+export default function MainForm() {
+  return (
+    <div>
+      <h3>Main Form</h3>
+      <form>
+        <ExampleSelect />
+      </form>
+
+    </div>
+  )
+}
