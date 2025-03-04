4727c778bd7fe0fbe6c3b70569f980cde85d76de
Eric Sorum <ejsorum@gmail.com>
2025-03-04 12:48:14 -0600

    Adjusted main container margin, width

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
   // }, []); // Empty dependency array to run once on mount
 
   return (
-    <div className="m-9 flex flex-col gap-9">
+    <div className="m-9 flex flex-col gap-9 max-w-3xl mx-auto">
       <div>
         <h1 className="text-center text-5xl font-bold m-3">Keg Curator</h1>
         <p className="text-center text-lg">Create the best selection of beer to serve in your bar or restaurant.</p>
