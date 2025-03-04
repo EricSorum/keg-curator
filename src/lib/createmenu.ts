13dbc772b117afe79c4d3f9c288d4914bde56773
Eric Sorum <ejsorum@gmail.com>
2025-03-04 12:38:53 -0600

    Created value algorithm.

--- src/lib/createmenu.ts
+++ src/lib/createmenu.ts
@@ -1,6 +1,8 @@
 import { Beer, FormResultsClass } from "./beers"
 import { selectBeers } from "./utils"
 
+
+
 export function createMenu(formResults: FormResultsClass, beerList: Beer[]) {
 
   const { businessName, numberOfHandles, minnesotaOnly, fanciness } = formResults;
@@ -13,6 +15,13 @@ export function createMenu(formResults: FormResultsClass, beerList: Beer[]) {
 
   // remove certain perecentage of fancy/budget beers...?
   // make results a certain percentage of budget/premium/presitige
+  // if num is 30 for example, 3/5 is premium, 2/5 is budget..
+
+  // or maybe there's just a window of 
+  // util that chooses if a certain beer should be fancy or not...
+  // so write util function that cycles between budget/premium/presitge
+  //. based on fanciness number.
+
   
 
   // Start off by choosing a few basic lagers and IPAs.
@@ -21,12 +30,12 @@ export function createMenu(formResults: FormResultsClass, beerList: Beer[]) {
 
 
 
-  menu = menu.concat(selectBeers("IPA", numberOfBasics, beerList, menu))
-  menu = menu.concat(selectBeers("Hazy IPA", numberOfBasics, beerList, menu))
-  menu = menu.concat(selectBeers("Lager", numberOfBasics, beerList, menu))
+  menu = menu.concat(selectBeers("IPA", numberOfBasics, beerList, menu, fanciness))
+  menu = menu.concat(selectBeers("Hazy IPA", numberOfBasics, beerList, menu, fanciness))
+  menu = menu.concat(selectBeers("Lager", numberOfBasics, beerList, menu, fanciness))
 
   const numberOfMiscBeers = numberOfHandles - menu.length;
-  menu = menu.concat(selectBeers("misc", numberOfMiscBeers, beerList, menu));
+  menu = menu.concat(selectBeers("misc", numberOfMiscBeers, beerList, menu, fanciness));
 
   return menu;
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,7 +15,7 @@ export function shuffle(arr: Beer[]) {
   return arr;
 }
 
-export function selectBeers(style: string, num: number, beerList: Beer[], menu: Beer[]) {
+export function selectBeers(style: string, num: number, beerList: Beer[], menu: Beer[], fanciness: number) : Beer[] {
   // Selects a certain number of beers of a certain style, or miscellaneous beers.
   let styleList: Beer[] = [];
   if (style === "misc") {
@@ -24,7 +24,25 @@ export function selectBeers(style: string, num: number, beerList: Beer[], menu: 
   } else {
     styleList = beerList.filter((beer) => beer.style === style && !menu.includes(beer));
   }
+
+  // Filter out all budget/prestige/premium based on results of fancinessFunc
+  const value = fancinessFunc(fanciness);
+  console.log(value)
+  styleList = styleList.filter((beer) => beer.value === value);
+
   const shuffleList: Beer[] = shuffle(styleList);
   const selections: Beer[] = shuffleList.slice(0, num);
   return selections;
+}
+
+export function fancinessFunc(fanciness: number) : string {
+  // Select whether the value should be budget, premium, or prestige, based on the fanciness input.
+  const randomNum = Math.floor(Math.random() * 100 * fanciness);
+  if (fanciness < 10 || randomNum < 100) {
+    return "Budget";
+  } else if (fanciness > 90 || randomNum > 1000) {
+    return "Prestige";
+  } else {
+    return "Premium";
+  }
 }
