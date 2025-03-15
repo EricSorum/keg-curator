e8e9e74aee62588fed5c321b76bca79611ec1197
Eric Sorum <ejsorum@gmail.com>
2025-03-15 12:00:30 -0500

    Migrated to Next

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Keg Curator',
+  description: 'Choose the best Minnesotan beer for your bar or restaurant.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <div id="root">{children}</div>
+        <script type="module" src="/src/main.tsx"></script>
+      </body>
+    </html>
+    )
+}
