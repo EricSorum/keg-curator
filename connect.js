2ca4ff13390295ee31157e4de8ed23239172fcf9
Eric Sorum <ejsorum@gmail.com>
2025-03-26 11:32:55 -0500

    Successfully connected to DB with connect.js

---
+++ connect.js
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const client = new MongoClient(process.env.MONGODB_URI);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Successfully connected to Atlas");
+
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
