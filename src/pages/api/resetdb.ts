be2fb1b8e57ce3ba3a581622b19e967b9d7d563e
Eric Sorum <ejsorum@gmail.com>
2025-06-26 19:00:54 -0500

    Updated beerjson, cleaned up resetdb

--- src/pages/api/resetdb.ts
+++ src/pages/api/resetdb.ts
@@ -5,55 +5,46 @@ import { readFile } from 'fs/promises';
 import path from 'path';
 
 type ResponseData = {
-  message: string
+  message: string;
 }
 
 export default async function resetDatabase(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
-) {
-  console.log('API endpoint called');
+  ) {
 
   async function getRawBeers(): Promise<Beer[]> {
     try {
       const beerListSrc = path.join(process.cwd(), 'src/lib/beerjson.json');
-      console.log('Attempting to read file from:', beerListSrc);
       
       const fileContent = await readFile(beerListSrc, 'utf-8');
-      console.log('File content length:', fileContent.length);
       
       const beers = JSON.parse(fileContent);
-      console.log('Parsed beers:', beers.length);
       return beers;
     } catch (error) {
-      console.error('Error reading beer JSON:', error);
       return [];
     }
   }
 
   try {
-    console.log('Attempting to connect to database...');
     await client.connect();
-    console.log('Database connected successfully');
     
     const db = client.db("keg_curator");
     const beerCollection = db.collection("beer_list");
     
-    const rawBeers = await getRawBeers();    
-    console.log('Number of beers to insert:', rawBeers?.length);
+    const rawBeers = await getRawBeers();
     
     if (rawBeers && rawBeers.length > 0) {
-      console.log('First beer object:', JSON.stringify(rawBeers[0], null, 2));
-      res.status(200).json({ message: "Database reset successful" });
+
+      const result = await beerCollection.insertMany(rawBeers);
+      console.log('Inserted documents:', result.insertedCount);
+      res.status(200).json({ message: "Database reset successful!" });
     } else {
-      console.log('No beers found in the data');
       res.status(400).json({ message: "No beers found to insert" });
     }
   } catch (error) {
-    console.error("Error resetting database:", error);
     res.status(500).json({ message: "Error resetting database" });
   } finally {
-    console.log('Closing database connection');
     await client.close();
   }
 } 
