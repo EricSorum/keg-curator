9472d30d50e01b14cb614a5e4f8bce1a0bdb85c5
Eric Sorum <ejsorum@gmail.com>
2025-06-27 07:44:45 -0500

    Switched createmenu to while loop.

--- src/lib/createmenu.ts
+++ src/lib/createmenu.ts
@@ -4,7 +4,7 @@ import picker from "./picker";
 
 export default function createMenu(formResults: FormResultsClass, beerList: Beer[]) {
 
-  const { numberOfHandles, minnesotaOnly, craftOnly, fanciness } = formResults;
+  const { numberOfHandles, minnesotaOnly, craftOnly, fanciness, cuisine } = formResults;
 
   let list: Beer[] = [...beerList]; // list needs to be a new array with the values of beerList
 
@@ -16,17 +16,30 @@ export default function createMenu(formResults: FormResultsClass, beerList: Beer
     list.sort(originCallback);
   }
 
+  if (cuisine.length) {
+    // list.sort(cuisineCallback(a: Beer, b: Beer, cuisine: string))
+  }
 
 
   let menu: Beer[] = [];
 
-  // const cuisineSelections = cuisineFunc();
+  // get array of cuisine and then cycle through them
+  // so then i can also splice each beer from list like i do in the while loop
+  // but i shouldn't repeat the splice operation... how do i get it in one while loop?
 
-  // maybe run cuisine just like picker... or maybe picker is what I need to be using here?
-  // menu.concat(cuisineFunc());
+  const cuisineSelections: Beer[] = cuisineFunc(formResults); 
 
-  for (let i = 0; i < numberOfHandles; i++) {
-    const newBeer = picker("misc", list, fanciness);
+  while (menu.length < numberOfHandles) {
+
+    let newBeer: Beer;
+
+    if (cuisineSelections.length) {
+      newBeer = cuisineSelections[0];
+      cuisineSelections.splice(0, 1)
+    } else {
+      newBeer = picker("misc", list, fanciness);
+    }
+
     menu.push(newBeer);
     // I avoid added duplicate beers by splicing each beer from the list.
     list.splice(list.indexOf(newBeer), 1);  
@@ -60,9 +73,14 @@ function originCallback(a: Beer, b: Beer): number { // Craft only
 function cuisineCallback(a: Beer, b: Beer, cuisine: string): number { // Cuisine
   if (a.cuisine === b.cuisine) {
     return 0;
-   } else if (a.cuisine === cuisine) {
+   } else if (a.cuisine.includes(cuisine)) {
   return -1;
   } else {
     return 1;
   }
+}
+
+function getCuisineSelection(arr: Beer[]) {
+  const returnBeer = arr[0];
+  arr
 }
