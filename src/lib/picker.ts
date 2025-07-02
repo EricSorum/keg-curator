72a7f7b453ec7408f6b1157ea30a21dc26fdeaca
Eric Sorum <ejsorum@gmail.com>
2025-07-01 21:53:58 -0500

    Brainstormed new picker solution.

--- src/lib/picker.ts
+++ src/lib/picker.ts
@@ -4,6 +4,7 @@ import { fancinessFunc, randomIndex } from './utils';
 export default function picker(style: string, beerList: Beer[], fanciness: number): Beer {
 
   const getFanciness: string = fancinessFunc(fanciness);
+  console.log(getFanciness)
   function findBeer(): Beer {
     // This makes an attempt to find a beer with both matching style and value.
 
@@ -12,6 +13,20 @@ export default function picker(style: string, beerList: Beer[], fanciness: numbe
     // let styleAndValue = beerList.slice(0, 15).find((beer) => beer.style === style && beer.value === getFanciness)
     let styleAndValue = beerList.slice(0, 15).find((beer) => beer.value === getFanciness)
     // Otherwise it just returns a matching style.
+
+    /*
+    what if we had an algorithm that put a score on each beer.
+    so this would be less efficinet but worth it for the results.
+    ok so each beer has a score.  W
+    fanciness can be a fraction - like if the user selects a low fanciness, the budget beers would be +2, 
+    but prestige would be -1
+    this value change can increment gradually with 
+
+    */
+
+    // Maybe whole function should just be picking random one 
+    //should only take fanciness into account, not anything else
+    // style will havet o be handled separately, similar to cuisine.
     if (styleAndValue) {
       return styleAndValue;
     } else  {
