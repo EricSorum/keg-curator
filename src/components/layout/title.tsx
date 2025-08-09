6118388d56a5354aaf5ff14bcbb111b9340354bd
Eric Sorum <ejsorum@gmail.com>
2025-08-09 13:03:49 -0500

    Added Title component.

---
+++ src/components/layout/title.tsx
@@ -0,0 +1,8 @@
+export default function Title() {
+  return (
+    <div>
+      <h1 className="text-center text-5xl font-bold m-3">Keg Curator</h1>
+      <p className="text-center text-lg">Create the best selection of beer to serve in your bar or restaurant.</p>
+    </div>
+  )
+}
