a705224b27a97860c832cc569a9fa8f9c85eb84c
Eric Sorum <ejsorum@gmail.com>
2025-08-10 15:38:20 -0500

    Decreased title font sizes.

--- src/components/layout/title.tsx
+++ src/components/layout/title.tsx
@@ -1,8 +1,8 @@
 export default function Title() {
   return (
-    <div>
-      <h1 className="text-center text-5xl font-bold m-3">Keg Curator</h1>
-      <p className="text-center text-lg">Create the best selection of beer to serve in your bar or restaurant.</p>
+    <div className="text-center">
+      <h1 className="text-3xl font-bold m-3">Keg Curator</h1>
+      <p className="text-md">Create the best selection of beer to serve in your bar or restaurant.</p>
     </div>
   )
 }
