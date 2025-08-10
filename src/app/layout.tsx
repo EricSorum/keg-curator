3e93f3cc87b009d2559fcea444a17e17053546c6
Eric Sorum <ejsorum@gmail.com>
2025-08-10 15:14:05 -0500

    Changed import names to kebab-case

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ export const metadata: Metadata = {
   description: 'Create the best selection of beer to serve in your bar or restaurant.',
 }
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/layout/AppSideBar"
+import { AppSidebar } from "@/components/layout/app-side-bar"
 
 export default function RootLayout({
   children,
