688214bfcc913121f88d35b6e4c2295c3d4ad68d
Eric Sorum <ejsorum@gmail.com>
2025-08-10 11:00:41 -0500

    Added sidebar to layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ export const metadata: Metadata = {
   title: 'Keg Curator',
   description: 'Create the best selection of beer to serve in your bar or restaurant.',
 }
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/layout/app-sidebar"
 
 export default function RootLayout({
   children,
@@ -12,7 +14,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div id="root">{children}</div>
+        <SidebarProvider>
+          <AppSidebar />
+          <main>
+            <SidebarTrigger />
+            <div id="root">{children}</div>
+          </main>
+        </SidebarProvider>
+
+
+
+        {/* <div id="root">{children}</div> */}
         {/* <script type="module" src="/src/main.tsx"></script> */}
       </body>
     </html>
