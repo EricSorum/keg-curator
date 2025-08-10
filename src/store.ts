18bcb3c248d41470de57286ebe08688ee6c38a7e
Eric Sorum <ejsorum@gmail.com>
2025-08-10 11:00:58 -0500

    Zustand store initial commit.

---
+++ src/store.ts
@@ -0,0 +1,14 @@
+import { create } from "zustand"
+import { FormResultsClass, defaultResults } from "./lib/beers"
+
+export interface ResultsState {
+  results: FormResultsClass,
+  setResults: (newResults: FormResultsClass) => void;  
+}
+
+export const useStore = create<ResultsState>((set) => ({
+  results: defaultResults,
+  setResults: (values) => set({ results: values }),
+}))
+
+// results needs to be set to default results
