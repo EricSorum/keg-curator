47a439a8d9436843a9d7474a849ecaaec32e198e
Eric Sorum <ejsorum@gmail.com>
2025-08-10 22:28:25 -0500

    Added pub handles, restyled beercard.

--- src/components/beer/beercard.tsx
+++ src/components/beer/beercard.tsx
@@ -1,5 +1,6 @@
 import { Beer } from '@/lib/beers';
 import { Card } from '../ui/card';
+import Image from 'next/image';
 
 type BeerCardProps = {
   index: number;
@@ -10,16 +11,28 @@ const BeerCard = ({index, beer}: BeerCardProps) => {
   // Increase index so it's not zero-indexed.
   index++;
   return (
-    <Card className="min-h-full">
-      <div className="m-3">
-        <h2>{index}. {beer.name}</h2> 
-        <p>Brewery: {beer.brewery}</p>
-        <p>Style: {beer.style}</p>
-        <p>Origin: {beer.origin}</p>
-        <p>Region: {beer.region}</p>
-        <p>Value: {beer.value}</p>
-      </div>
-    </Card>
+    <div className="relative min-w-[200px]">
+      <Image
+        src="/pubhandle.png"
+        width={60}
+        height={120}
+        alt="Beer Selection"
+        className="mx-auto"
+      />
+      <Card className="rounded-[3rem] h-[120px] absolute top-[40px] w-full">
+        <div className="m-3 text-center">
+          <h3 className="text-lg text-bold tracking-tight">
+            {/* {index}.  */}
+            {beer.name}
+          </h3>
+          <p>{beer.brewery}</p>
+          <p>{beer.style}</p>
+          {/* <p>Origin: {beer.origin}</p>
+          <p>Region: {beer.region}</p>
+          <p>Value: {beer.value}</p> */}
+        </div>
+      </Card>
+    </div>
   );
 };
 
