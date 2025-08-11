62e762265035a9f0a518ab8ed7317d9aa919c68b
Eric Sorum <ejsorum@gmail.com>
2025-08-10 19:18:25 -0500

    Changed layout folder to sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ export const metadata: Metadata = {
   description: 'Create the best selection of beer to serve in your bar or restaurant.',
 }
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/layout/app-side-bar"
+import { AppSidebar } from "@/components/sidebar/app-side-bar"
 
 export default function RootLayout({
   children,
--- src/components/layout/title.tsx
+++
@@ -1,8 +0,0 @@
-export default function Title() {
-  return (
-    <div className="text-center">
-      <h1 className="text-3xl font-bold m-3">Keg Curator</h1>
-      <p className="text-md">Create the best selection of beer to serve in your bar or restaurant.</p>
-    </div>
-  )
-}
