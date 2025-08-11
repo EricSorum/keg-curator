5618a399d9546baf4aef0f324ace821465d4cdcd
Eric Sorum <ejsorum@gmail.com>
2025-08-10 19:16:28 -0500

    Added sidebar components to Title

---
+++ src/components/sidebar/title.tsx
@@ -0,0 +1,15 @@
+import { SidebarGroup, SidebarGroupContent } from "../ui/sidebar"
+
+export default function Title() {
+  return (
+    <SidebarGroup>
+      <SidebarGroupContent>
+        <div className="text-center">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-700 m-3">Keg Curator</h1>
+          <p className="text-md">Create the best selection of beer to serve in your bar or restaurant.</p>
+        </div>
+      </SidebarGroupContent>
+    </SidebarGroup>    
+
+  )
+}
