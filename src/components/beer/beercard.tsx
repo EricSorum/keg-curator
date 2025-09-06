39a148305059fefcf211b6b65f76a143221dfa3a
Eric Sorum <ejsorum@gmail.com>
2025-09-06 08:21:57 -0500

    Added brewerySlug to match logo img

--- src/components/beer/beercard.tsx
+++ src/components/beer/beercard.tsx
@@ -11,30 +11,35 @@ type BeerCardProps = {
 const BeerCard = ({index, beer}: BeerCardProps) => {
   // Increase index so it's not zero-indexed.
   index++;
+    // const breweryName = "montucky"
 
   // CURRENTLY FINDING WAY TO DYNAMICALLY RENDER IMAGE SOURCE
-  const breweryName = beer.brewery.trim();
-  const imgSrc: string = "./public/brewery-logos/" + {breweryName} + ".webp"
-  const altString = beer.brewery + " logo"
+  const brewerySlug = beer.brewery.toLowerCase().replace(/[^a-z]/gi, "");
+  console.log(brewerySlug)
+  const imgSrc = `/brewery-logos/${brewerySlug}.webp`;
+  const altString = `${beer.brewery} logo`;
+
+
 
   return (
-    <div className="relative min-w-[200px]">
+    <div className="relative w-[200px]">
       <Image
         src="/pubhandle.png"
         width={60}
         height={120}
         alt="Beer Selection"
-        className="mx-auto drop-shadow-lg"
+        className="mx-auto drop-shadow-lg w-full"
       />
       <Card className="rounded-[20%] absolute top-[40px] w-full bg-amber-300 border-amber-950 border-b-[4px] border-r-[4px]">
         <div className="m-3 text-center font-serif text-amber-950">
+          {brewerySlug.length && 
           <Image 
             src={imgSrc}
-            width={60}
-            height={50}
+            width={120}
+            height={90}
             alt={altString}
             className="mx-auto drop-shadow-lg"
-          />
+          />}
           <h3 className="text-xl text-bold tracking-tight">
             {beer.name}
           </h3>
