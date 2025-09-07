e78d5a7a1d46fd15ec8303a2216d40e6b8fcc3b0
Eric Sorum <ejsorum@gmail.com>
2025-09-07 10:47:10 -0500

    Added ext check.

--- scripts/convertImages.js
+++ scripts/convertImages.js
@@ -6,19 +6,11 @@ import path from "path";
 const inputFolder = "./raw-images";
 const outputFolder = "./public/brewery-logos";
 
-// async function getData() {
-//   const filePath = path.join(process.cwd(), './scripts/data/beerjson.json');
-//   const fileContent = await fsp.readFile(filePath, 'utf-8');
-//   return JSON.parse(fileContent);
-// }
-
 async function convertImages() {
   if (!fs.existsSync(outputFolder || !fs.existsSync(inputFolder))) {
     console.error('Requires /raw-images and /public/brewery-logos folders')
     return
   }
-  // const data = await getData();
-  // console.log(data[0]);
 
   const rawImages = await fsp.readdir(inputFolder);
 
@@ -27,19 +19,21 @@ async function convertImages() {
     const fileName = rawImages[i];
     const ext = path.extname(fileName).toLowerCase();
 
+    if(ext === ".webp") {
+      continue;
+    }
 
     if (![".jpg", ".jpeg", ".png", ".gif", ".avif", ".svg"].includes(ext)) {
       console.error("Requires jpb, jpeg, png, gif, or avif format")
       continue;
     }
 
-    // still need to find way to make file names match brewery names
-
     const inputPath = path.join(inputFolder, fileName);
     const outputPath = path.join(outputFolder, `${path.parse(fileName).name}.webp`);
     try {
       await sharp(inputPath)
         .resize({ width: 100 })
+        // .resize({ height: 100 })
         .webp({ quality: 80 })
         .toFile(outputPath)     
         console.log(`Converted ${fileName} to ${path.parse(fileName).name}.webp`);
