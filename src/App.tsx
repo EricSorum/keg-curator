70c9c6e85f9fea068439f170eff873d401ec60fd
Eric Sorum <ejsorum@gmail.com>
2025-09-07 10:48:51 -0500

    Cleanded up unused code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
-// import { useEffect, useState } from 'react'
 import Results from './components/main-form/results'
-// import Title from './components/layout/title'
-// import Navbar from './components/layout/navbar'
-// import Footer from './components/layout/Footer'
-// import { number } from 'zod';
-
-
 
 function App() {
-  // const [beerList, setBeerList] = useState(); // State to hold the beer list
-
-  // useEffect(() => {
-  //   const fetchBeerList = async () => {
-  //     const beers = await BeerList(); // Await the Promise to get the resolved array
-  //     setBeerList(beers); // Set the state with the resolved array
-  //   };
-
-  //   fetchBeerList(); // Call the function to fetch the beer list
-  // }, []); // Empty dependency array to run once on mount
 
   return (
-    
     <div className="m-9 max-w-4xl mx-auto">
-      {/* <Navbar /> */}
-
       <div className="m-9 flex flex-col gap-9 max-w-3xl mx-auto">
-        {/* <Title /> */}
-        {/* <hr></hr> */}
-        {/* <MainForm /> */}
         <Results />
-        {/* <Footer /> */}
       </div>
     </div>
   )
