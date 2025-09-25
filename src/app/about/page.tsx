7c09c796373ca9d5dde2889ef05713a44749c4ab
Eric Sorum <ejsorum@gmail.com>
2025-09-25 07:49:41 -0500

    Gave page correct title.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import TitleCard from "@/components/cards/titlecard"
 
-export default function Tech() {
+export default function About() {
 
   
   return (
