e4e2b1848f64d30c5fbb6e4f2ca9d097e98f3775
Eric Sorum <ejsorum@gmail.com>
2025-09-25 07:51:50 -0500

    Added preferred brewery bonus to scoring.

--- src/lib/calculateScore.ts
+++ src/lib/calculateScore.ts
@@ -1,43 +1,23 @@
-import { fancinessFunc } from "./utils";
+import { fancinessFunc } from "./fancinessFunc";
 import Beer from "@/models/Beer";
 import FormResultsClass from "@/models/FormResults";
+import cuisineFunc from "./cuisineFunc";
+import { preferredBreweries } from "./constants";
+import { bonus } from "./utils";
 
 export default function calculateScore(beer: Beer, formResults: FormResultsClass) {
   const { minnesotaOnly, craftOnly, fanciness, chosenCuisine } = formResults;
   const { name, brewery, origin, region, value, cuisine } = beer;
 
-  const calculateCuisineScore = (): number => {
-    let returnScore = 0;
-    const imports = ["Japanese", "Latin American"];
-
-    // Shared makes sure that the above imports are exclusive.
-    // i.e. there won't likely be Japanese beers alongside Latin American beers.
-    const shared = imports.filter(e => cuisine.includes(e));
-
-    // need another filter:
-    // if the beer is not latin american, beers from mexico should not be included
-    if (cuisine.length < 3 || chosenCuisine.length < 3) {
-      returnScore = 0;
-    } else if (region === "Japan" && chosenCuisine !== "Japananese") {
-      return -2;
-    } else if (region === "Mexico" && chosenCuisine !== "Latin American") {
-      return -2;
-    } else if (shared.length && !cuisine.includes(chosenCuisine)) {
-      returnScore = -2;
-    } else if (cuisine.includes(chosenCuisine)) {
-      returnScore = 2;
-    }
-    return returnScore;
-  }
-
   // maybe instead of fanciness func, we can just weight values here
 
   const scoreObj = {
-    // breweryScore: preferredBreweries.includes(brewery) ? 1 : 0,
-    originScore: craftOnly && origin === "Craft" ? 2 : 0,
-    regionScore: minnesotaOnly && region === "Minnesota" ? 2 : 0,
-    valueScore: value === fancinessFunc(fanciness) ? 2 : 0,
-    cuisineScore: calculateCuisineScore()
+    originScore: craftOnly && origin === "Craft" ? 2 : -2,
+    regionScore: minnesotaOnly && region === "Minnesota" ? 2 : -2,
+    valueScore: value === fancinessFunc(fanciness) ? 1 : 0,
+    cuisineScore: cuisineFunc(region, cuisine, chosenCuisine),
+    preferredScore: preferredBreweries.includes(brewery) ? bonus(40) : 0,
+    blackStackBonus: name === "Local 755" ? bonus(50) : 0
   };
   
   // Add up each value in scoreObj
